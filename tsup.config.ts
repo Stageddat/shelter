4905efe371983b12c4f502c82695efa4e28e8a86
Stageddat <chmarc72@gmail.com>
2025-03-06 22:43:10 +0100

    feat: update tsup configuration to use ESM format for output

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
 	entry: ['./src/**/*.ts'],
-	format: 'cjs',
+	format: 'esm',
 	clean: true,
 	minify: true,
 	outDir: 'dist',
 	bundle: false,
-	esbuildOptions(options, ctx) {
+	esbuildOptions(options) {
 		options.outbase = './src';
 	},
 	dts: false,
