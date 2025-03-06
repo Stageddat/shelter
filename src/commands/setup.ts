ab3d04d74debe2c637a5ff8d0f2f17c11f1d8b65
Stageddat <chmarc72@gmail.com>
2025-03-07 00:15:57 +0100

    feat: enhance setup command to handle user registration status with appropriate responses

--- src/commands/setup.ts
+++ src/commands/setup.ts
@@ -2,6 +2,7 @@ import { CommandInteraction, SlashCommandBuilder } from 'discord.js';
 import { registerController } from '@/controllers/register';
 import { errorEmbed } from '@/views/general';
 import { Logger } from '@/lib/logger';
+import { RegisterStatus } from '@/enum/registerStatus';
 
 const setupCommand = {
 	data: new SlashCommandBuilder()
@@ -12,9 +13,16 @@ const setupCommand = {
 			userID: interaction.user.id,
 		});
 		try {
-			if (response) {
-				return interaction.reply('{ embeds: [response.embed] }');
+			if (typeof response === 'number') {
+				return interaction.reply('user not registered switchiong to step');
 			}
+			switch (response) {
+				case RegisterStatus.userNotRegistered:
+					return interaction.reply('welcome bruh');
+				case RegisterStatus.userSetupComplete:
+					return interaction.reply("you're already setup");
+			}
+
 			return interaction.reply({ embeds: [errorEmbed] });
 		} catch (error) {
 			Logger.error(error);
