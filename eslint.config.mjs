11db87021f7d7d06c33f0ec320b09982f5b54d0e
Stageddat <chmarc72@gmail.com>
2025-03-07 00:15:51 +0100

    chore: update ESLint and Prettier configurations for improved code formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
+// eslint.config.mjs
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -10,16 +13,19 @@ export default [
 			globals: globals.browser,
 			ecmaVersion: 'latest',
 		},
+		plugins: {
+			prettier: eslintPluginPrettier,
+		},
 		rules: {
+			indent: 'off',
 			'arrow-spacing': ['warn', { before: true, after: true }],
-			'comma-dangle': ['error', 'always-multiline'],
-			'comma-spacing': 'error',
-			'comma-style': 'error',
+			'comma-dangle': 'off',
+			'comma-spacing': 'off',
+			'comma-style': 'off',
 			curly: ['error', 'multi-line', 'consistent'],
 			'dot-location': ['error', 'property'],
 			'handle-callback-err': 'off',
-			indent: ['error', 'tab'],
-			'keyword-spacing': 'error',
+			'keyword-spacing': 'off',
 			'max-nested-callbacks': ['error', { max: 4 }],
 			'max-statements-per-line': ['error', { max: 2 }],
 			'no-console': 'off',
@@ -27,32 +33,26 @@ export default [
 			'no-floating-decimal': 'error',
 			'no-inline-comments': 'error',
 			'no-lonely-if': 'error',
-			'no-multi-spaces': 'error',
-			'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 1, maxBOF: 0 }],
+			'no-multi-spaces': 'off',
+			'no-multiple-empty-lines': 'off',
 			'no-shadow': ['error', { allow: ['err', 'resolve', 'reject'] }],
-			'no-trailing-spaces': 'error',
+			'no-trailing-spaces': 'off',
 			'no-var': 'error',
 			'no-undef': 'off',
-			'object-curly-spacing': ['error', 'always'],
+			'object-curly-spacing': 'off',
 			'prefer-const': 'error',
-			quotes: ['error', 'single'],
-			semi: ['error', 'always'],
-			'space-before-blocks': 'error',
-			'space-before-function-paren': [
-				'error',
-				{
-					anonymous: 'never',
-					named: 'never',
-					asyncArrow: 'always',
-				},
-			],
-			'space-in-parens': 'error',
-			'space-infix-ops': 'error',
-			'space-unary-ops': 'error',
+			quotes: 'off',
+			semi: 'off',
+			'space-before-blocks': 'off',
+			'space-before-function-paren': 'off',
+			'space-in-parens': 'off',
+			'space-infix-ops': 'off',
+			'space-unary-ops': 'off',
 			'spaced-comment': 'error',
 			yoda: 'error',
 		},
 	},
 	pluginJs.configs.recommended,
 	...tseslint.configs.recommended,
+	prettierConfig,
 ];
