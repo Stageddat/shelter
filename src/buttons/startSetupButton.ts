92d00cc5cbffda10245deced1de72a5417b89b8f
Stageddat <chmarc72@gmail.com>
2025-03-11 00:25:24 +0100

    feat: enhance error handling in startSetupButton and implement logging

--- src/buttons/startSetupButton.ts
+++ src/buttons/startSetupButton.ts
@@ -1,11 +1,18 @@
 import { ButtonInteraction } from 'discord.js';
+import { Logger } from 'src/lib/logger';
+import { errorEmbed } from 'src/views/general';
+
+// para futuro yo, mover la logica de si es userSetupComplete a controlador y no en view
 
 export default {
 	customId: 'startSetupButton',
 	execute: async (interaction: ButtonInteraction) => {
-		return await interaction.reply({
-			content: 'Setting up!',
-			ephemeral: true,
-		});
+		await interaction.deferReply();
+		try {
+			// editar depediendo del step actual + 1
+		} catch (error) {
+			Logger.error(error);
+			return await interaction.followUp({ embeds: [errorEmbed] });
+		}
 	},
 };
