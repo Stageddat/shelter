cbcb88feed5c4a413e79854a253ea3c35c8a7252
Stageddat <chmarc72@gmail.com>
2025-03-10 19:16:13 +0100

    feat: change rin pfp link and add completed setup embed

--- src/views/register/registerEmbeds.ts
+++ src/views/register/registerEmbeds.ts
@@ -1,3 +1,4 @@
+import { rinProfilePicture } from 'config/links';
 import { EmbedBuilder } from 'discord.js';
 
 export class registerEmbedsView {
@@ -7,10 +8,23 @@ export class registerEmbedsView {
 			.setTitle(`welcome ${username}!`)
 			.setAuthor({
 				name: 'Rin',
-				iconURL: 'https://shelter-d.b-cdn.net/rin-pfp.jpg',
+				iconURL: rinProfilePicture,
 			})
 			.setDescription(
 				'hello there, random internet user!\nim Rin and welcome to **shelter**, your special little corner on discord. here you can write your daily adventures and save them in your personal diary, right on the platform you love so much!',
+			);
+	}
+
+	static setupCompletedEmbed() {
+		return new EmbedBuilder()
+			.setColor(0x5eddff)
+			.setTitle('wopaa')
+			.setAuthor({
+				name: 'Rin',
+				iconURL: rinProfilePicture,
+			})
+			.setDescription(
+				'looks like u already finished setting up, start with `/addEntry` or user `/help` for more info!',
 			);
 	}
 }
