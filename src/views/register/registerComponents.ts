794b28612a51abcc8ed9f5574966e7bff07d591f
Stageddat <chmarc72@gmail.com>
2025-04-04 23:33:07 +0200

    feat: add timezone buttons to the registration component

--- src/views/register/registerComponents.ts
+++ src/views/register/registerComponents.ts
@@ -15,3 +15,40 @@ const nextIntroButton = new ButtonBuilder()
 	.setEmoji('🛩️');
 
 export const introRow = new ActionRowBuilder<ButtonBuilder>().addComponents(nextIntroButton);
+
+const addTimezoneButton = new ButtonBuilder()
+	.setCustomId('addTimezoneButton')
+	// .setLabel('')
+	.setStyle(ButtonStyle.Secondary)
+	.setEmoji('<add:1357025119352131775>');
+
+const restartTimezoneButton = new ButtonBuilder()
+	.setCustomId('restartTimezoneButton')
+	// .setLabel('🔄')
+	.setStyle(ButtonStyle.Secondary)
+	.setEmoji('🔄');
+
+const substractTimezoneButton = new ButtonBuilder()
+	.setCustomId('substractTimezoneButton')
+	// .setLabel('')
+	.setStyle(ButtonStyle.Secondary)
+	.setEmoji('<substract:1357025109277413619>');
+
+export const setTimezoneRow = new ActionRowBuilder<ButtonBuilder>().addComponents(
+	addTimezoneButton,
+	restartTimezoneButton,
+	substractTimezoneButton,
+);
+
+// const timeZones = Intl.supportedValuesOf('timeZone');
+// const selectTimezone = new StringSelectMenuBuilder()
+// 	.setCustomId('timezoneSelect')
+// 	.setPlaceholder('choose your timezone!')
+// 	.addOptions(
+// 		new StringSelectMenuOptionBuilder().setLabel('test1').setValue('test1'),
+// 		new StringSelectMenuOptionBuilder().setLabel('test2').setValue('2'),
+// 	);
+
+// export const selectTimezoneRow = new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
+// 	selectTimezone,
+// );
